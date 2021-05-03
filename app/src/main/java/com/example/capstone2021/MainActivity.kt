d4d4341bf3abef148e27ebec7752c1ec345d48c9
leedoyeon <gcdalee@gmail.com>
2021-05-03 22:38:25 +0900

    Development Exifdata Extraction

--- app/src/main/java/com/example/capstone2021/MainActivity.kt
+++ app/src/main/java/com/example/capstone2021/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.capstone2021
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.array
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val PICK_IMAGE_REQUEST = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun loadImagefromGallery(v: View) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+    }
+
+    //버전분기 필요함
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val intent = Intent(applicationContext, ExifActivity::class.java)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
+            try{
+                val uri: Uri? = data.data
+                intent.putExtra("uri", uri.toString())
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+                //이미지 리사이징
+                val nh = (bitmap.height * (1024.0 / bitmap.width)).toInt()
+                val scaled = Bitmap.createScaledBitmap(bitmap, 1024, nh, true)
+                val imgView : ImageView = findViewById(R.id.showImageView)
+                imgView.setImageBitmap(scaled)
+
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        } else {
+            Log.d("Activity Result", "something wrong")
+        }
+
+    }
+
 }
+
