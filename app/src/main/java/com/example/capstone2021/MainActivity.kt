95b55d44faad35edecaaff9b98b67190cc3e1bf5
Gang <1552419@gmail.com>
2021-05-19 00:28:26 +0900

    스위치 값 테스트

--- app/src/main/java/com/example/capstone2021/MainActivity.kt
+++ app/src/main/java/com/example/capstone2021/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.capstone2021
 
-import android.R.array
 import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import androidx.annotation.RequiresApi
+import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.capstone2021.ExifActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,19 +19,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 
     fun loadImagefromGallery(v: View) {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = MediaStore.Images.Media.CONTENT_TYPE
         startActivityForResult(intent, PICK_IMAGE_REQUEST)
+
     }
 
     //버전분기 필요함
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        val ExifSW = findViewById<Switch>(R.id.LoadExif)
+        val VisionSW = findViewById<Switch>(R.id.LoadVision)
+        val textview : TextView = findViewById(R.id.textview)
+        textview.setText("$ExifSW | $VisionSW")
         val intent = Intent(applicationContext, ExifActivity::class.java)
-
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
             try{
                 val uri: Uri? = data.data
