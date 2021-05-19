0898484385d4cd709a5ea6568bd2213de65bdb29
Gang <1552419@gmail.com>
2021-05-19 18:08:45 +0900

    visionAI : Main - migration

--- app/src/main/java/com/example/capstone2021/MainActivity.kt
+++ app/src/main/java/com/example/capstone2021/MainActivity.kt
@@ -1,78 +1,321 @@
 package com.example.capstone2021
 
+import android.Manifest
 import android.content.Intent
+import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
-import android.view.View
-import android.widget.CompoundButton
-import android.widget.Switch
-import android.widget.TextView
+import android.support.design.widget.FloatingActionButton
+import androidx.core.content.FileProvider
+import android.support.v7.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.json.GoogleJsonResponseException
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.vision.v1.Vision
+import com.google.api.services.vision.v1.VisionRequest
+import com.google.api.services.vision.v1.VisionRequestInitializer
+import com.google.api.services.vision.v1.model.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.IOException
+import java.lang.ref.WeakReference
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
 
     private val PICK_IMAGE_REQUEST = 1
+    private val CLOUD_VISION_API_KEY = BuildConfig.API_KEY
+    val FILE_NAME = "temp.jpg"
+    private val ANDROID_CERT_HEADER = "X-Android-Cert"
+    private val ANDROID_PACKAGE_HEADER = "X-Android-Package"
+    private val MAX_LABEL_RESULTS = 10
+    private val MAX_DIMENSION = 1200
 
+    private val TAG = MainActivity::class.java.simpleName
+    private val GALLERY_PERMISSIONS_REQUEST = 0
+    private val GALLERY_IMAGE_REQUEST = 1
+    val CAMERA_PERMISSIONS_REQUEST = 2
+    val CAMERA_IMAGE_REQUEST = 3
+
+    private var mImageDetails: TextView? = null
+    private var mMainImage: ImageView? = null
+
+    //Main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sw1 = findViewById<Switch>(R.id.LoadExif)
-        val sw2 = findViewById<Switch>(R.id.LoadVision)
-        val textview : TextView = findViewById(R.id.textview)
-
-        sw1.setOnCheckedChangeListener { buttonView, isChecked ->
-            sw1.toggle()
-            textview.text = "select sw1"
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+            builder
+                .setMessage(R.string.dialog_select_prompt)
+                .setPositiveButton(R.string.dialog_select_OCR,{ dialog, which -> startGalleryChooser() })
+//                .setNegativeButton(R.string.dialog_select_Exif,{ dialog, which -> copyFiles() })
+            builder.create().show()
         }
-        sw2.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(sw2.isChecked == true){
-                sw2.toggle()
-                sw2.isChecked = false
-                textview.text = "sw2 true to false"
-            }
-            else{
-                sw2.toggle()
-                sw2.isChecked = true
-                textview.text = "sw2 false to true"
-            }
-        }
-
-    }
-    inner class checkboxListener : CompoundButton.OnCheckedChangeListener{
-        override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
-            TODO("Not yet implemented")
-        }
+        mImageDetails = findViewById(R.id.image_details)
+        mMainImage = findViewById(R.id.main_image)
     }
 
-    fun loadImagefromGallery(v: View) {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = MediaStore.Images.Media.CONTENT_TYPE
-        startActivityForResult(intent, PICK_IMAGE_REQUEST)
-
+    fun startGalleryChooser() {
+        if (PermissionUtils.requestPermission(
+                this,
+                GALLERY_PERMISSIONS_REQUEST,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(
+                Intent.createChooser(intent, "Select a photo"),
+                GALLERY_IMAGE_REQUEST
+            )
+        }
     }
 
-    //버전분기 필요함
+
+    fun getCameraFile(): File? {
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File(dir, FILE_NAME)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            uploadImage(data.data)
+        }
+    }
+    fun onRequestPermissionsResult(
+        requestCode: Int, @NonNull permissions: Array<String?>?, @NonNull grantResults: IntArray?
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions!!, grantResults!!)
+        when (requestCode) {
+                    GALLERY_PERMISSIONS_REQUEST -> if (PermissionUtils.permissionGranted(
+                    requestCode,
+                    GALLERY_PERMISSIONS_REQUEST,
+                    grantResults
+                )
+            ) {
+                startGalleryChooser()
+            }
+        }
+    }
 
-        val intent = Intent(applicationContext, ExifActivity::class.java)
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
-            try{
-                val uri: Uri? = data.data
-                intent.putExtra("uri", uri.toString())
-                startActivity(intent)
-            } catch (e: Exception) {
-                e.printStackTrace()
+    //start vision API
+    fun uploadImage(uri: Uri?) {
+        if (uri != null) {
+            try {
+                // scale the image to save on bandwidth
+                val bitmap = scaleBitmapDown(
+                    MediaStore.Images.Media.getBitmap(contentResolver, uri),
+                    MAX_DIMENSION
+                )
+                callCloudVision(bitmap)
+                mMainImage!!.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                Log.d(TAG, "Image picking failed because " + e.message)
+                Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show()
             }
         } else {
-            Log.d("Activity Result", "something wrong")
+            Log.d(TAG, "Image picker gave us a null image.")
+            Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun prepareAnnotationRequest(bitmap: Bitmap): Vision.Images.Annotate {
+        val httpTransport: HttpTransport = AndroidHttp.newCompatibleTransport()
+        val jsonFactory: JsonFactory = GsonFactory.getDefaultInstance()
+        val requestInitializer: VisionRequestInitializer =
+            object : VisionRequestInitializer(CLOUD_VISION_API_KEY) {
+                /**
+                 * We override this so we can inject important identifying fields into the HTTP
+                 * headers. This enables use of a restricted cloud platform API key.
+                 */
+                @Throws(IOException::class)
+                override fun initializeVisionRequest(visionRequest: VisionRequest<*>) {
+                    super.initializeVisionRequest(visionRequest)
+                    val packageName = packageName
+                    visionRequest.requestHeaders[ANDROID_PACKAGE_HEADER] = packageName
+                    val sig: String = PackageManagerUtils.getSignature(packageManager, packageName)
+                    visionRequest.requestHeaders[ANDROID_CERT_HEADER] = sig
+                }
+            }
+        val builder = Vision.Builder(httpTransport, jsonFactory, null)
+        builder.setVisionRequestInitializer(requestInitializer)
+        val vision = builder.build()
+        val batchAnnotateImagesRequest = BatchAnnotateImagesRequest()
+        batchAnnotateImagesRequest.requests = object : ArrayList<AnnotateImageRequest?>() {
+            init {
+                val annotateImageRequest = AnnotateImageRequest()
+
+                // Add the image
+                val base64EncodedImage = Image()
+                // Convert the bitmap to a JPEG
+                // Just in case it's a format that Android understands but Cloud Vision
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream)
+                val imageBytes = byteArrayOutputStream.toByteArray()
+
+                // Base64 encode the JPEG
+                base64EncodedImage.encodeContent(imageBytes)
+                annotateImageRequest.image = base64EncodedImage
+
+                // add the features we want
+                annotateImageRequest.features = object : ArrayList<Feature?>() {
+                    init {
+                        val textDetection = Feature()
+                        textDetection.type = "TEXT_DETECTION"
+                        textDetection.maxResults = MAX_LABEL_RESULTS
+                        add(textDetection)
+                    }
+                }
+
+                // Add the list of one thing to the request
+                add(annotateImageRequest)
+            }
+        }
+        val annotateRequest = vision.images().annotate(batchAnnotateImagesRequest)
+        // Due to a bug: requests to Vision API containing large images fail when GZipped.
+        annotateRequest.disableGZipContent = true
+        Log.d(TAG, "created Cloud Vision request object, sending request")
+        return annotateRequest
+    }
+
+    private class LableDetectionTask internal constructor(
+        activity: MainActivity,
+        annotate: Vision.Images.Annotate
+    ) :
+        AsyncTask<Any?, Void?, String>() {
+        private val mActivityWeakReference: WeakReference<MainActivity>
+        private val mRequest: Vision.Images.Annotate
+        protected override fun doInBackground(vararg params: Any): String {
+            try {
+                Log.d(TAG, "created Cloud Vision request object, sending request")
+                val response = mRequest.execute()
+                return convertResponseToString(response)
+            } catch (e: GoogleJsonResponseException) {
+                Log.d(TAG, "failed to make API request because " + e.content)
+            } catch (e: IOException) {
+                Log.d(
+                    TAG, "failed to make API request because of other IOException " +
+                            e.message
+                )
+            }
+            return "Cloud Vision API request failed. Check logs for details."
         }
 
+        override fun onPostExecute(result: String) {
+            val activity = mActivityWeakReference.get()
+            if (activity != null && !activity.isFinishing) {
+                val imageDetail: TextView = activity.findViewById(R.id.image_details)
+                imageDetail.text = result
+            }
+        }
+
+        init {
+            mActivityWeakReference = WeakReference(activity)
+            mRequest = annotate
+        }
     }
+
+    private fun callCloudVision(bitmap: Bitmap) {
+        // Switch text to loading
+        mImageDetails.setText(R.string.loading_message)
+
+        // Do the real work in an async task, because we need to use the network anyway
+        try {
+            val labelDetectionTask: AsyncTask<Any, Void, String> =
+                LableDetectionTask(this, prepareAnnotationRequest(bitmap))
+            labelDetectionTask.execute()
+        } catch (e: IOException) {
+            Log.d(
+                TAG, "failed to make API request because of other IOException " +
+                        e.message
+            )
+        }
+    }
+
+    private fun scaleBitmapDown(bitmap: Bitmap, maxDimension: Int): Bitmap {
+        val originalWidth = bitmap.width
+        val originalHeight = bitmap.height
+        var resizedWidth = maxDimension
+        var resizedHeight = maxDimension
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension
+            resizedWidth =
+                (resizedHeight * originalWidth.toFloat() / originalHeight.toFloat()).toInt()
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension
+            resizedHeight =
+                (resizedWidth * originalHeight.toFloat() / originalWidth.toFloat()).toInt()
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension
+            resizedWidth = maxDimension
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false)
+    }
+
+    private fun convertResponseToString(response: BatchAnnotateImagesResponse): String? {
+        val message = StringBuilder("I found these things:\n\n")
+        val text = response.responses[0].textAnnotations
+        if (text != null) {
+            for (label in text) {
+                message.append(String.format(Locale.US, "%s", label.description))
+                message.append("\n")
+            }
+        } else {
+            message.append("nothing")
+        }
+        return message.toString()
+    }
+//
+//    fun loadImagefromGallery(v: View) {
+//        val intent = Intent(Intent.ACTION_PICK)
+//        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+//        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+//
+//    }
+//
+//    //버전분기 필요함
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        val intent = Intent(applicationContext, ExifActivity::class.java)
+//        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
+//            try{
+//                val uri: Uri? = data.data
+//                intent.putExtra("uri", uri.toString())
+//                startActivity(intent)
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        } else {
+//            Log.d("Activity Result", "something wrong")
+//        }
+//
+//    }
 
 }
 
